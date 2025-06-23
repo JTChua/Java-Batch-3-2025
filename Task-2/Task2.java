52447d9f501c7ec645035af816e3758c566a0494
Jotham <54047006+JTChua@users.noreply.github.com>
2025-06-23 03:35:55 +0000

    Task2

---
+++ Task-2/Task2.java
@@ -0,0 +1,10 @@
+public class Task2 {
+    public static void main(String[] args) {
+
+        int zero = 0;
+        String output = "W" + zero + "w";
+
+        System.out.println(output);
+
+    }
+}
