56b923e5f4a0bb419b3e04dcb54781f2c6657ccd
Jotham <54047006+JTChua@users.noreply.github.com>
2025-06-23 03:39:36 +0000

    update task2

--- Task-2/Task2.java
+++ Task-2/Task2.java
@@ -1,10 +1,19 @@
 public class Task2 {
     public static void main(String[] args) {
 
-        int zero = 0;
-        String output = "W" + zero + "w";
-
-        System.out.println(output);
+        // Create primitives with specific values
+        byte b = 0;
+        short s = 3110;
+        int i = 0;
+        float f = 2.0f;
+        char c = ' ';
+        boolean bool = true;
+        
+        // Concatenate them to form the desired output
+        String output = "H" + s + c + "w" + b + "rld" + c + f + c + bool;
+        
+        // Print the result
+        System.out.println(output);  // Output: H3110 w0rld 2.0 true
 
     }
 }
