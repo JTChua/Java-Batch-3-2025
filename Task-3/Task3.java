e47a8ec02db577b1f890f0dc673ccda987a5a5ff
Jotham <54047006+JTChua@users.noreply.github.com>
2025-06-28 00:15:37 +0000

    Creating Task-3 for Task3.java

---
+++ Task-3/Task3.java
@@ -0,0 +1,16 @@
+public class Task3 {
+
+  public static void main(String[] args) {
+
+    String a = new String("Wow");
+    String b = "Wow";
+    String c = a;
+    String d = c;
+
+    System.out.println(a);
+    System.out.println(b);
+    System.out.println(c);
+    System.out.println(d);
+
+  }
+}
