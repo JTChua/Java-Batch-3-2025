dabca1d2ce756d8d3d4e016c07eb66d7486a5757
Jotham <54047006+JTChua@users.noreply.github.com>
2025-06-28 02:08:28 +0000

    fix first section to resolved success

--- Task-3/Task3.java
+++ Task-3/Task3.java
@@ -2,30 +2,14 @@ public class Task3 {
 
   public static void main(String[] args) {
 
-    String a = new String("Wow");
+    String a = "Wow";
     String b = "Wow";
-    String c = a;
+    String c = a + "!";
     String d = c;
 
-    System.out.println(a);
-    System.out.println(b);
-    System.out.println(c);
-    System.out.println(d);
-
-    System.out.println("\n");
-
-    // boolean b1 = a == b;
-    boolean b1 = a.contentEquals(b);
-    System.out.println(b1);
-
-    // boolean b2 = d.equals(b + "!");
-    boolean b2 = d.equals(b);
-    System.out.println(b2);
-
-    boolean b3 = c.contains(a);
-    System.out.println(b3);
-
-    System.out.println("\n");
+    boolean b1 = a == b;
+    boolean b2 = d.equals(b + "!");
+    boolean b3 = !c.equals(a);
 
     if (b1 && b2 && b3) {
       System.out.println("Success!");
