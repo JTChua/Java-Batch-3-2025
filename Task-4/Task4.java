fb48420170b9ebe86fcde226f1dececba13b0d4b
Jotham <54047006+JTChua@users.noreply.github.com>
2025-06-30 03:36:39 +0000

    Task4.java is created

--- Task-4/Task4.java
+++ Task-4/Task4.java
@@ -1,10 +1,29 @@
+import java.util.Scanner;
 
 public class Task4 {
 
   public static void main(String[] args) {
 
-    String name = "MariaChristina";
-    System.out.println(name);
+    // Create a Scanner object to read input
+    Scanner scanner = new Scanner(System.in);
+
+    // Prompt the user to enter a string
+    System.out.println("Enter a string to check if it is a palindrome:");
+    String input = scanner.next();
+
+    // Create a StringBuilder object and reverse the input string
+    StringBuilder stringBuilder = new StringBuilder(input);
+    String reversedString = stringBuilder.reverse().toString();
+
+    // Check if the input string is a palindrome
+    if (input.equals(reversedString)) {
+      System.out.println("The input string is a palindrome.");
+    } else {
+      System.out.println("The input string is not a palindrome.");
+    }
+
+    // Close the scanner
+    scanner.close();
 
   }
 
