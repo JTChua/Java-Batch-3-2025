536e6c66daf49da728a2e11ee5b53f8a37ba971e
Jotham <54047006+JTChua@users.noreply.github.com>
2025-07-12 03:28:10 +0000

    enable junit.jupiter and updater MainTest.java

--- FaceToFaceDay6/test/MainTest.java
+++ FaceToFaceDay6/test/MainTest.java
@@ -2,6 +2,15 @@ package FaceToFaceDay6.test;
 
 import FaceToFaceDay6.src.TwoSum;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.*;
+
 public class MainTest {
+
+  @Test
+  void findSumIndices() {
+
+  }
 
 }
